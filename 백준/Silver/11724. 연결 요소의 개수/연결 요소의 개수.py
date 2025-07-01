69c0ea40707e54a578d0a686278e6d443bbefd72
minak <dev.minhyuk@gmail.com>
2025-07-01 14:39:37 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 724 ms, Memory: 67044 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,36 +1,44 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
 
 N, M = list(map(int, input().split()))
 
-adj = [[] for _ in range(N)]
+adj = [[] for _ in range(N+1)]
 
 for _ in range(M):
   u, v = list(map(int, input().split()))
 
-  adj[u-1].append(v-1)
-  adj[v-1].append(u-1)
+  adj[u].append(v)
+  adj[v].append(u)
 
-visit = [False] * N
+visit = [False] * (N+1)
 count = 0
 
-for i in range(N):
+for i in range(1, N+1):
+  # 만약 i가 방문한 위치라면 카운트 다음으로 넘어가기.
   if visit[i]:
     continue
 
   count += 1
-
   queue = deque([i])
-  visit[i] = True
 
+  # 그게 아니면 카운트를 1을 올리고,
+  # 거기 관련된 모든곳을 True 처리로 해야함.
+  # 큐에 인덱스를 넣고, 그 인덱스를 지우면서 그 인덱스에 들어있는 
+  # 인접 리스트를 전부 큐에 넣고 큐를 지우면서 방문을 추가하는 형태로 작업
   while len(queue) != 0:
     u = queue.popleft()
 
     for v in adj[u]:
       if not visit[v]:
         queue.append(v)
-        visit[v] = True 
-
+        visit[v] = True
+  
 print(count)
+
+
+
+
+
+
